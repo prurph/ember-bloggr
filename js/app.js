abe2c4747860398bfc3d970cb40df93a2f326aa3
Prescott Murphy <prurph@gmail.com>
2014-04-02 17:07:00 -0400

    Add post editing

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@ App = Ember.Application.create(); // Ember application instance
 // for the application
 App.Router.map(function() {
   this.resource("about");
-  this.resource("posts");
-  this.resource("post", { path: ":post_id"}); // specify how post_id goes into URL
+  this.resource("posts", function() {
+    this.resource("post", { path: ":post_id"}); // specify how post_id goes into URL
+  });
 });
 
 // route specifies which model a template should be backed by
@@ -14,6 +15,34 @@ App.PostsRoute = Ember.Route.extend({
   model: function() {
     return posts;
   }
+});
+
+// when we go to /posts/1 params has ":post_id" from path: declaration above
+// so just find the post by the "id" property that matches params.post_id
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy("id", params.post_id);
+  }
+});
+
+// in Ember, controller is object that stores application state and responds to
+// events from template
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false, // not persistent (so don't save on model; if refresh turn off edit)
+
+  actions: {
+    edit: function() {
+      this.set("isEditing", true);
+    },
+
+    doneEditing: function() {
+      this.set("isEditing", false);
+    }
+  }
+});
+
+Ember.Handlebars.helper("format-date", function(date) {
+  return moment(date.fromNow());
 });
 
 // this would be data from the backend
